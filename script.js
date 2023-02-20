ee475699a5b54cbd7e6a26d74c47bdecce0a5a77
JHIR01 <henryirivere@gmail.com>
2023-02-20 14:28:42 -0500

    I am finished with creating the function playRound(), this function will play a round of the game and say a winner!

--- script.js
+++ script.js
@@ -4,7 +4,42 @@ console.log("Hello world!");
 //      - This is how the computer will play the game.
 
 let getComputerChoice = () => {
-    return Math.floor(Math.random() * (4 - 1) + 1);
+    tempNum = Math.floor(Math.random() * (4 - 1) + 1);
+    if (tempNum === 1) {
+        return 'Rock';
+    } else if (tempNum === 2) {
+        return 'Paper';
+    } else if (tempNum === 3) {
+        return 'Scissors'
+    }
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+// playRound() - This function will take in two arguements and return a statement on who won the game.
+
+let playRound = (playerSelection, computerSelection) => {
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
+        return 'You lose! Paper beats rock!'
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
+        return 'You win! Paper beats rock!'
+    }
+    if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissor') {
+        return 'You lose! scissors beats paper!'
+    } else if (playerSelection.toLowerCase() === 'scissor' && computerSelection.toLowerCase() === 'paper') {
+        return 'You win! Scissors beats paper!'
+    }
+    if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'rock') {
+        return 'You lose! Rock beats scissors!'
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
+        return 'You win! Rock beats scissors!'
+    }
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return 'Draw! You can the computer had the same item!'
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log("This is the computer's choice: " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
